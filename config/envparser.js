2f77492c585e2a4d7b9e0944431454b08c9280e1
John Williamson <johnjwilliamson@live.com>
2019-04-15 12:12:30 +1000

    t2

--- config/envparser.js
+++ config/envparser.js
@@ -2,6 +2,9 @@ const DotEnv = require('dotenv')
 
 module.exports = function (isDevelopment) {
   console.log('maybe might', process.env)
+  if (isDevelopment) {
+
+  
   const parsedEnv = DotEnv.config({ path: isDevelopment ? '.env.development' : '.env.production'}).parsed
   // Let's stringify our variables
   for (key in parsedEnv) {
@@ -10,4 +13,5 @@ module.exports = function (isDevelopment) {
     }
   }
   return parsedEnv
+}
 }
